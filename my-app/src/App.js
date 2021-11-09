b6cb3f26817db4042420b6398d016d862555a2e7
mirza samsin baig <mirzasamsinbaig143@gmail.com>
2021-11-09 22:08:46 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import './assets/css/animate.css';
+import './assets/css/aos.css';
+import './assets/css/bootstrap.min.css';
+// import './assets/css/icons.css';
+import './assets/css/main.css';
+import './assets/css/normalize.css';
+import './assets/css/shake.css';
+import './assets/css/style.css';
+import './assets/css/swiper.min.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
